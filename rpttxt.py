9209d091d6328fdbb90e4a9eeb131679ffb18550
nendo_code <nendocode@gmail.com>
2020-05-17 22:46:15 +0900

    First commit

---
+++ rpttxt.py
@@ -0,0 +1,16 @@
+import sys
+import csv
+
+template = sys.argv[1]
+datafile = sys.argv[2]
+
+with open(template,"r",encoding = "utf-8") as t:
+    template_data = t.read()
+
+with open(datafile,"r",encoding = "utf-8") as d:
+    f = csv.DictReader(d, dialect=csv.excel)
+    for row in f:
+        output = template_data
+        for k in row.keys():
+            output = output.replace(k,row[k])
+        print(output)
